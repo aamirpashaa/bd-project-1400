f2bb2be4ecaa7510e9f98997921f98a3cd7cc979
heidari <heidari@mail.ir>
2021-07-06 20:48:14 +0430

    better script to run services and scripts

--- run.sh
+++ run.sh
@@ -1,15 +1,41 @@
 #!/bin/bash
 
+start_services() 
+{
+	./packages/kafka/bin/zookeeper-server-start.sh ./packages/kafka/config/zookeeper.properties &
+	./packages/kafka/bin/kafka-server-start.sh ./packages/kafka/config/server.properties &
+}
 
-trap "exit" INT TERM ERR
-trap "kill 0" EXIT
+start_scripts()
+{
+	. venv/bin/activate
+	python3 collect_data/fetch_data.py & 
+	python3 preprocess/preprocess.py & 
+	python3 persistence/redis_manager.py &
+	python3 ./server.py < /dev/null > /dev/null 2>&1 &
+}
 
-. venv/bin/activate
-./packages/kafka/bin/zookeeper-server-start.sh ./packages/kafka/config/zookeeper.properties &
-./packages/kafka/bin/kafka-server-start.sh ./packages/kafka/config/server.properties &
-python3 collect_data/fetch_data.py & 
-python3 preprocess/preprocess.py & 
-python3 persistence/redis_manager.py & 
-python3 server.py &
 
-wait
+stop_all() 
+{
+	killall python3
+
+	./packages/kafka/bin/kafka-server-stop.sh
+	./packages/kafka/bin/zookeeper-server-stop.sh
+}
+
+
+
+case "$1" in
+	"start-services")
+		start_services
+		;;
+
+	"start-scripts")
+		start_scripts
+		;;
+
+	"stop")
+		stop_all
+		;;
+esac
