a507ad318915e3e7893c1b852d11eef8849a3876
Amir Pasha Motamed <a.motamed@ut.ac.ir>
2021-07-30 08:07:59 +0000

    Adding function for fetching history by start day and fixing crashes

--- collect_data/fetch_history.py
+++ collect_data/fetch_history.py
@@ -12,25 +12,50 @@ def date_format(message):
 producer = KafkaProducer(bootstrap_servers=['localhost:9092'], api_version=(0, 10), value_serializer=lambda v: json.dumps(v, default=date_format).encode('utf-8'))
 
 
-def get_messages_at_date(ch, days, end_date):
+def get_messages_by_days(ch, days, end_date):
 	for chat in ch:
-		for new_message in client.iter_messages(chat, offset_date=end_date):
-			if (end_date - (new_message.date).replace(tzinfo=None) ).days > days:
-				break
+		try:
+			for new_message in client.iter_messages(chat, offset_date=end_date):
+				if (end_date - (new_message.date).replace(tzinfo=None) ).days > days:
+					break
 
-			channel = client.get_entity(new_message.peer_id.channel_id)
-			new_message = new_message.to_dict()
-			new_message['sender_name'] = channel.title
-			
-			producer.send('preprocess', new_message)
-			producer.flush()
+				channel = client.get_entity(new_message.peer_id.channel_id)
+				new_message = new_message.to_dict()
+				new_message['sender_name'] = channel.title
+				
+				producer.send('preprocess', new_message)
+				producer.flush()
 
-			print(datetime.datetime.now(),"new message inserted into preprocess")
+				print(datetime.datetime.now(),"new message inserted into preprocess")
+		except:
+			continue
+
+def get_messages_by_start_date(ch, start_date, end_date):
+	for chat in ch:
+		try:
+			for new_message in client.iter_messages(chat, offset_date=end_date):
+				if (new_message.date).replace(tzinfo=None) < start_date:
+					break
+
+				channel = client.get_entity(new_message.peer_id.channel_id)
+				new_message = new_message.to_dict()
+				new_message['sender_name'] = channel.title
+				
+				producer.send('preprocess', new_message)
+				producer.flush()
+
+				print(datetime.datetime.now(),"new message inserted into preprocess")
+		except:
+			continue
 
 new_client = Client()
 client = new_client.connect()
 channels_list = new_client.get_channels()
 
-days = 10
+# days = 10
+# end_time = datetime.datetime.now()
+# get_messages_by_days(ch = channels_list, days=days, end_date=end_time)
+
 end_time = datetime.datetime.now()
-get_messages_at_date(ch = channels_list, days=days, end_date=end_time)
+start_date = datetime.datetime(2021,7,21)
+get_messages_by_start_date(ch = channels_list, start_date=start_date, end_date=end_time)
